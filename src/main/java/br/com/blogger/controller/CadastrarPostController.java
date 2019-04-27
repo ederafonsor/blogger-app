ab4957f4a64f4a5cef2acfc3658ac8a1d278be28
Eder Afonso Rabelo <ederafonsor@gmail.com>
2019-04-27 17:07:37 -0300

    PostBe Added, PostBe and UserBe conections redirected to heroku

---
+++ src/main/java/br/com/blogger/controller/CadastrarPostController.java
@@ -0,0 +1,49 @@
+package br.com.blogger.controller;
+
+import br.com.blogger.model.post.PostBe;
+import br.com.blogger.model.post.PostVo;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class CadastrarPostController {
+
+    private PostVo postVo;
+    private PostBe postBe;
+
+    public CadastrarPostController() {
+        if (postVo == null) {
+            postVo = new PostVo();
+        }
+    }
+
+    public void cadastrarPost() { 
+          try {
+            
+           getPostBe().cadastrarPost(postVo);
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario Salvo", null);
+            FacesContext.getCurrentInstance().addMessage(null, fm);
+        } catch (Exception e) {
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null);
+            FacesContext.getCurrentInstance().addMessage(null, fm);
+        }
+    }
+
+    public PostVo getPostVo() {
+        return postVo;
+    }
+
+    public void setPostVo(PostVo postVo) {
+        this.postVo = postVo;
+    }
+
+    public PostBe getPostBe() {
+        if (this.postBe == null) {
+            this.postBe = new PostBe();
+        }
+        return postBe;
+    }
+
+    public void setPostBe(PostBe postBe) {
+        this.postBe = postBe;
+    }
+}
---
+++ src/main/java/br/com/blogger/model/post/PostBe.java
@@ -0,0 +1,30 @@
+package br.com.blogger.model.post;
+
+import br.com.blogger.features.persistence.PersistenceProperties;
+import java.io.Serializable;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class PostBe implements Serializable {
+
+    public void cadastrarPost(PostVo postVo) {
+
+        EntityManagerFactory fabricaConexao = Persistence.createEntityManagerFactory("BLOG_PG_PU", new PersistenceProperties().getConfigPersistence());
+        // EntityManagerFactory fabricaConexao = Persistence.createEntityManagerFactory("BLOG_PG_PU"); //Conexão local
+
+        EntityManager conexao = fabricaConexao.createEntityManager();
+
+        EntityTransaction tx = conexao.getTransaction();
+        tx.begin();
+
+        conexao.persist(postVo);
+
+        tx.commit();
+
+        conexao.close();
+        fabricaConexao.close();
+    }
+
+}
